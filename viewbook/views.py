fcca241f99dc243f5eb8d22320afb75a67f8fd86
SupaStuff <StuffQilla@gmail.com>
2015-11-30 15:44:05 -0500

    removed bad import

--- viewbook/views.py
+++ viewbook/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import Context
-from django.db import books
+#...
+#from django.db import books
 
 # Create your views here.
 def renderviewbook(request, book_id):
