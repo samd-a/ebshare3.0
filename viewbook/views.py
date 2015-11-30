606507e04c3aeadcab280cafadde6114fa1f0d8c
SupaStuff <StuffQilla@gmail.com>
2015-11-30 15:39:18 -0500

    made a book model and tried to add 'fixtures,' default data

--- viewbook/views.py
+++ viewbook/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import Context
+from django.db import books
 
 # Create your views here.
 def renderviewbook(request, book_id):
