999a2581d203836fbfbab836754d2919b1986ad8
SupaStuff <StuffQilla@gmail.com>
2015-12-08 19:53:26 -0500

    fixed viewbook/urls.py

--- viewbook/models.py
+++ viewbook/models.py
@@ -4,7 +4,7 @@ from books.models import book
 import os
 
 class reader(models.Model):
-    user = ForeignKey(User, db_column='user')
+    user = models.ForeignKey(User, db_column='user')
     book = models.ForeignKey(book)
     time_read = models.IntegerField(default=0)
     time_left = models.IntegerField(default=0)
--- viewbook/urls.py
+++ viewbook/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     #url(r'^$', views.renderviewbook, name='viewbook'),
     #for passing parameters. Might need dir_name/(...
     url(r'^(?P<book_id>[0-9]+)$', views.renderviewbook, name='viewbook'),
-    url(r'^/reader/(?P<book_id>[0-9]+)$', views.renderreader, name='reader'),
+    url(r'^reader/(?P<book_id>[0-9]+)$', views.renderreader, name='reader'),
 ]
